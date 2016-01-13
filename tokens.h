7b54ff68d35c0577d3b7cd19f3c6068fe2c48538
elegantmetal <conlan2012@msn.com>
2016-01-13 00:39:49 +0000

    Finished with lab quiz.

--- tokens.h
+++ tokens.h
@@ -11,22 +11,9 @@
 // Date: Dec 30, 2015
 //
 
-#define IF 1000
-#define ENDIF 1001
-#define ELSE 1002
-#define WHILE 1003
-#define PRINT 1004
-#define INT 1006
-#define FLOAT 1007
-#define CHAR 1008
-#define STRUCT 1009
-#define RETURN 1010
-#define INT_VAL 1011
-#define FLOAT_VAL 1012
-#define IDENTIFIER 1013
-#define OPERATOR 1014
-#define EQUALS 1015
-#define AND 1016
-#define OR 1017
-#define JUNK_TOKEN 2000
+#define MY 1000
+#define TOKEN 1001
+#define BLOB1 1002
+#define BLOB2 1003
+#define JUNK 2000
 
